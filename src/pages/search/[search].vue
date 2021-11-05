aaf584ed61d946434b664188010d8c63a0ec48b1
6n3 <92433450+6n3@users.noreply.github.com>
2021-11-05 15:03:01 +0100

    fix

--- src/pages/search/[search].vue
+++ src/pages/search/[search].vue
@@ -5,6 +5,7 @@ export default {
 	mounted() {
 		try {
 			if (this.$route.params.search.startsWith('TX_')) this.$router.replace('/transaction/' + this.$route.params.search)
+			else throw new Error()
 		}
 		catch {
 			this.$router.replace('/explorer/' + this.$route.params.search)
